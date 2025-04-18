ad4455d8a11234d2c1238eb066aa1b5d356b0758
Hellager <steinsgu@gmail.com>
2025-04-19 00:05:10 +0800

    fix: check pin script error issue

--- Wincent/ScriptStrategy.cs
+++ Wincent/ScriptStrategy.cs
@@ -251,7 +251,7 @@ namespace Wincent
                 {ShellApplicationSetup}
                 $shellApplication.Namespace($scriptPath).Self.InvokeVerb('pintohome')                
 
-                $isWin11 = (Get-CimInstance -Class Win32_OperatingSystem).Caption -Match ""Windows 11""
+                $isWin11 = (Get-CimInstance -Class Win32_OperatingSystem).Caption -Match 'Windows 11'
                 if ($isWin11)
                 {{
                     $shellApplication.Namespace('{parameter}').Self.InvokeVerb('pintohome')
