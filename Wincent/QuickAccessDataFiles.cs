59dc6032291c7f716b5fa0ebc9b626d6bb802d29
Hellager <steinsgu@gmail.com>
2025-04-18 22:58:51 +0800

    feat: update QuickAccessDataFiles

--- Wincent/QuickAccessDataFiles.cs
+++ Wincent/QuickAccessDataFiles.cs
@@ -18,32 +18,71 @@ namespace Wincent
     /// </summary>
     public class RealFileSystem : IFileSystem
     {
-        public bool FileExists(string path) => File.Exists(path);
-        public void DeleteFile(string path) => File.Delete(path);
-        public DateTime GetLastWriteTime(string path) => File.GetLastWriteTime(path);
+        public bool FileExists(string path)
+        {
+            return File.Exists(path);
+        }
+
+        public void DeleteFile(string path)
+        {
+            File.Delete(path);
+        }
+
+        public DateTime GetLastWriteTime(string path)
+        {
+            return File.GetLastWriteTime(path);
+        }
     }
 
     /// <summary>
-    /// Windows Quick Access data file management and information
+    /// Quick Access data files management interface
     /// </summary>
-    public class QuickAccessDataFiles
+    public interface IQuickAccessDataFiles
+    {
+        /// <summary>
+        /// Gets modification time for script-related data file
+        /// </summary>
+        /// <param name="scriptType">Script type</param>
+        /// <returns>Modification timestamp</returns>
+        DateTime GetModifiedTimeForScript(PSScript scriptType);
+
+        /// <summary>
+        /// Removes recent files data file
+        /// </summary>
+        void RemoveRecentFile();
+
+        /// <summary>
+        /// Gets path to recent files data in Quick Access
+        /// </summary>
+        string RecentFilesPath { get; }
+
+        /// <summary>
+        /// Gets path to frequent folders data in Quick Access
+        /// </summary>
+        string FrequentFoldersPath { get; }
+    }
+
+    /// <summary>
+    /// Windows Quick Access data file management
+    /// </summary>
+    public class QuickAccessDataFiles : IQuickAccessDataFiles
     {
         private readonly string _recentFilesPath;
         private readonly string _frequentFoldersPath;
         private readonly IFileSystem _fileSystem;
 
         /// <summary>
-        /// Creates Quick Access data file manager using real file system
+        /// Initializes new instance of <see cref="QuickAccessDataFiles"/>
         /// </summary>
-        /// <exception cref="InvalidPathException">Thrown when unable to retrieve Windows Recent folder path</exception>
-        public QuickAccessDataFiles() : this(new RealFileSystem()) { }
+        public QuickAccessDataFiles()
+            : this(new RealFileSystem())
+        {
+        }
 
         /// <summary>
-        /// Creates Quick Access data file manager with specified file system
+        /// Initializes new instance of <see cref="QuickAccessDataFiles"/>
         /// </summary>
-        /// <param name="fileSystem">File system operations interface</param>
-        /// <exception cref="InvalidPathException">Thrown when unable to retrieve Windows Recent folder path</exception>
-        /// <exception cref="ArgumentNullException">Thrown when fileSystem parameter is null</exception>
+        /// <param name="fileSystem">File system interface</param>
         public QuickAccessDataFiles(IFileSystem fileSystem)
         {
             _fileSystem = fileSystem ?? throw new ArgumentNullException(nameof(fileSystem));
@@ -56,16 +95,18 @@ namespace Wincent
         }
 
         /// <summary>
-        /// Retrieves Windows Recent folder path
+        /// Gets path to Windows Recent folder
         /// </summary>
-        /// <returns>Path to Recent folder</returns>
+        /// <returns>Recent folder path</returns>
         /// <exception cref="InvalidPathException">Thrown when path retrieval fails</exception>
         private string GetWindowsRecentFolder()
         {
             string recentFolder = Environment.GetFolderPath(Environment.SpecialFolder.Recent);
-            return string.IsNullOrEmpty(recentFolder)
-                ? throw new InvalidPathException("Windows Recent Folder", "Failed to retrieve Windows Recent folder path")
-                : recentFolder;
+            if (string.IsNullOrEmpty(recentFolder))
+            {
+                throw new InvalidPathException("Windows Recent Folder", "Unable to retrieve Windows Recent folder path");
+            }
+            return recentFolder;
         }
 
         /// <summary>
@@ -78,7 +119,10 @@ namespace Wincent
             try
             {
                 if (_fileSystem.FileExists(path))
+                {
                     _fileSystem.DeleteFile(path);
+                }
+                // If file doesn't exist, no exception is thrown
             }
             catch (Exception ex) when (ex is IOException || ex is UnauthorizedAccessException)
             {
@@ -87,75 +131,140 @@ namespace Wincent
         }
 
         /// <summary>
-        /// Deletes Recent Files data file (ignores if file doesn't exist)
+        /// Removes recent files data file (ignores if file doesn't exist)
         /// </summary>
-        /// <exception cref="IOException">Thrown when file deletion fails</exception>
-        public void RemoveRecentFile() => RemoveFile(_recentFilesPath);
+        /// <exception cref="IOException">Thrown when deletion fails</exception>
+        public void RemoveRecentFile()
+        {
+            RemoveFile(_recentFilesPath);
+        }
 
         /// <summary>
-        /// Gets last modified time of Recent Files data
+        /// Gets modification time of recent files data
         /// </summary>
-        /// <returns>Last modified timestamp</returns>
-        public DateTime GetRecentFilesModifiedTime() => GetModifiedTime(_recentFilesPath);
+        /// <returns>Modification timestamp</returns>
+        /// <exception cref="FileNotFoundException">Thrown when file doesn't exist</exception>
+        public DateTime GetRecentFilesModifiedTime()
+        {
+            if (_fileSystem.FileExists(_recentFilesPath))
+            {
+                try
+                {
+                    return _fileSystem.GetLastWriteTime(_recentFilesPath);
+                }
+                catch (Exception ex)
+                {
+                    // Capture and rethrow access errors
+                    throw new IOException($"Failed to get file timestamp: {_recentFilesPath}", ex);
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException($"Recent files data file not found: {_recentFilesPath}", _recentFilesPath);
+            }
+        }
 
         /// <summary>
-        /// Gets last modified time of Frequent Folders data
+        /// Gets modification time of frequent folders data
         /// </summary>
-        /// <returns>Last modified timestamp</returns>
-        public DateTime GetFrequentFoldersModifiedTime() => GetModifiedTime(_frequentFoldersPath);
+        /// <returns>Modification timestamp</returns>
+        /// <exception cref="FileNotFoundException">Thrown when file doesn't exist</exception>
+        public DateTime GetFrequentFoldersModifiedTime()
+        {
+            if (_fileSystem.FileExists(_frequentFoldersPath))
+            {
+                try
+                {
+                    return _fileSystem.GetLastWriteTime(_frequentFoldersPath);
+                }
+                catch (Exception ex)
+                {
+                    throw new IOException($"Failed to get file timestamp: {_frequentFoldersPath}", ex);
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException($"Frequent folders data file not found: {_frequentFoldersPath}", _frequentFoldersPath);
+            }
+        }
 
         /// <summary>
-        /// Gets latest modified time across Quick Access data files
+        /// Gets modification time of Quick Access data
         /// </summary>
-        /// <returns>Newest timestamp from both data files</returns>
+        /// <returns>Modification timestamp</returns>
+        /// <exception cref="FileNotFoundException">Thrown when no data files exist</exception>
         public DateTime GetQuickAccessModifiedTime()
         {
-            DateTime recentTime = GetRecentFilesModifiedTime();
-            DateTime frequentTime = GetFrequentFoldersModifiedTime();
+            DateTime recentTime = DateTime.MinValue;
+            DateTime frequentTime = DateTime.MinValue;
+            bool anyFileExists = false;
+
+            try
+            {
+                recentTime = GetRecentFilesModifiedTime();
+                anyFileExists = true;
+            }
+            catch (FileNotFoundException)
+            {
+                // Ignore missing recent files
+            }
+
+            try
+            {
+                frequentTime = GetFrequentFoldersModifiedTime();
+                anyFileExists = true;
+            }
+            catch (FileNotFoundException)
+            {
+                // Ignore missing frequent folders
+            }
+
+            if (!anyFileExists)
+            {
+                throw new FileNotFoundException("No available Quick Access data files");
+            }
+
             return recentTime > frequentTime ? recentTime : frequentTime;
         }
 
         /// <summary>
-        /// Gets modified time based on script type
+        /// Gets modification time for specific script type
         /// </summary>
-        /// <param name="scriptType">Script type identifier</param>
-        /// <returns>Corresponding data file modified time</returns>
+        /// <param name="scriptType">Script type</param>
+        /// <returns>Related data file modification time</returns>
+        /// <exception cref="FileNotFoundException">Thrown when target data file is missing</exception>
         public DateTime GetModifiedTimeForScript(PSScript scriptType)
-        {
-            switch (scriptType)
-            {
-                case PSScript.QueryRecentFile:
-                    return GetRecentFilesModifiedTime();
-                case PSScript.QueryFrequentFolder:
-                    return GetFrequentFoldersModifiedTime();
-                case PSScript.QueryQuickAccess:
-                    return GetQuickAccessModifiedTime();
-                default:
-                    return DateTime.Now; // Use current time for non-query scripts
-            }
-        }
-
-        private DateTime GetModifiedTime(string path)
         {
             try
             {
-                return _fileSystem.FileExists(path)
-                    ? _fileSystem.GetLastWriteTime(path)
-                    : DateTime.Now;
+                switch (scriptType)
+                {
+                    case PSScript.QueryRecentFile:
+                        return GetRecentFilesModifiedTime();
+                    case PSScript.QueryFrequentFolder:
+                        return GetFrequentFoldersModifiedTime();
+                    case PSScript.QueryQuickAccess:
+                        return GetQuickAccessModifiedTime();
+                    default:
+                        return DateTime.Now; // Use current time for non-query scripts
+                }
             }
-            catch
+            catch (FileNotFoundException) when (scriptType != PSScript.QueryRecentFile &&
+                                               scriptType != PSScript.QueryFrequentFolder &&
+                                               scriptType != PSScript.QueryQuickAccess)
             {
+                // Return current time for non-specific query scripts
                 return DateTime.Now;
             }
         }
 
         /// <summary>
-        /// Path to Recent Files data file
+        /// Gets path to recent files data
         /// </summary>
         public string RecentFilesPath => _recentFilesPath;
 
         /// <summary>
-        /// Path to Frequent Folders data file
+        /// Gets path to frequent folders data
         /// </summary>
         public string FrequentFoldersPath => _frequentFoldersPath;
     }
